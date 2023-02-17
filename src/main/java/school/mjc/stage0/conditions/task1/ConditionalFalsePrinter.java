bb15318d93be9ec9354c5130094be9bea0a4f020
unknown <vladmain29@gmail.com>
2023-02-17 11:11:08 +0300

    more tasks

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (true == false) {
+        } else {
+            System.out.println(false);
+        }
     }
 }
